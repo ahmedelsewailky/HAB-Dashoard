05526a29f602ec344f6b80ba898b7f8a0f8ebdd0
ahmedelsewailky <ahmedelsewailky@gmail.com>
2025-12-09 22:24:11 +0200

    Dealing with navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
-    console.log('Hello! jQuery...');
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("qty-btn")) {
+        let parent = e.target.closest(".cart-qty-control");
+        let valueEl = parent.querySelector(".qty-value");
+        let current = parseInt(valueEl.innerText);
+
+        if (e.target.classList.contains("plus")) {
+            current++;
+        } else if (e.target.classList.contains("minus")) {
+            current = Math.max(1, current - 1); // ماينقصش عن 1
+        }
+
+        valueEl.innerText = current;
+    }
 });
