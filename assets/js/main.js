291a31264cf0caccb4845766dcef7386bbd763d7
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2025-12-28 14:14:30 +0200

    Refactor sidebar collapsing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
-document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("qty-btn")) {
-        let parent = e.target.closest(".cart-qty-control");
-        let valueEl = parent.querySelector(".qty-value");
-        let current = parseInt(valueEl.innerText);
+$(function () {
+    "use strict";
 
-        if (e.target.classList.contains("plus")) {
-            current++;
-        } else if (e.target.classList.contains("minus")) {
-            current = Math.max(1, current - 1); // ماينقصش عن 1
-        }
-
-        valueEl.innerText = current;
-    }
+    $(".collapsed-sidebar .sidebar")
+        .on("mouseenter", function () {
+            $("body").addClass("visible-sidebar");
+        })
+        .on("mouseleave", function () {
+            $("body").removeClass("visible-sidebar");
+        });
 });
